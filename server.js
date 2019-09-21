100abcd0308136ca995f80cfe181a4d603816af9
Ted Robinson <tfqbtd@aol.com>
2019-09-21 13:30:32 -0500

    added server update

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const path = require('path');
-
+import express, { static } from 'express';
+import { join } from 'path';
+import cors from 'cors';
 const app = express();
 
+app.use(cors());
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{tasc-assmt-tedrob}}'));
+app.use(static(__dirname + '/dist/tasc-assmt-tedrob'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname, '/dist/{{tasc-assmt-tedrob}}/index.html'));
+  res.sendFile(join(__dirname + '/dist/tasc-assmt-tedrob/index.html}}'));
 });
 
 // Start the app by listening on the default Heroku port

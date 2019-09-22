063d6b793b6beaba5c8ab21e182174d6407fe467
Ted Robinson <tfqbtd@aol.com>
2019-09-22 16:31:04 -0500

    update server js

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
-import express, { static } from 'express';
-import { join } from 'path';
-import cors from 'cors';
-const app = express();
+// import express, { static } from 'express';
+// import { join } from 'path';
+// import cors from 'cors';
+// const app = express();
+const express = require('express'),
+      app     = express(),
+      path    = require('path'),
+      cors    = require('cors'),
+      Port    = process.env.Port || 8080;
 
-app.use(cors());
-// Serve only the static files form the dist directory
-app.use(static(__dirname + '/dist/tasc-assmt-tedrob'));
-
-app.get('/*', function (req, res) {
-
-  res.sendFile(join(__dirname + '/dist/tasc-assmt-tedrob/index.html'));
+express((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-type,' +
+    'Accept');
+    next;
 });
+const tt = 'dist/tasc-assmt-tedrob/index.html/'
+express(() => {
+  console.log('testing', `${tt}`);
+})
+  .use(express.static(path.join(__dirname, 'favicon.ico')))
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(bodyParser.json({
+    type: 'application/json'
+  }))
+  .get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/tasc-assmt-tedrob/index.html'))
+  })
+  .use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  })
+  .listen(Port, () => {
+    console.log(`listening on ${port} test ${tt}`);
+  })
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// app.use(cors());
+// // Serve only the static files form the dist directory
+// app.use(static(__dirname + '/dist/tasc-assmt-tedrob'));
+
+// app.get('/*', function (req, res) {
+
+//   res.sendFile(join(__dirname + '/dist/tasc-assmt-tedrob/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);

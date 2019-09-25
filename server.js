414a62054cde661a4cd6148b272f543242f21f29
Ted Robinson <tfqbtd@aol.com>
2019-09-25 10:29:23 -0500

    update server js

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-express((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-type,' +
-    'Accept');
-  next;
-});
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'dist/tasc-assmt-tedrob/index.html')));
-app.use(express.static(path.join(__dirname, 'dist/tasc-assmt-tedrob/favicon.ico')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/tasc-assmt-tedrob/index.html'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/tasc-assmt-tedrob'));
+
+// Send all requests to index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/tasc-assmt-tedrob/index.html'));
 });
 
-app.listen(port, cors(), () => {
+// default Heroku PORT
+app.listen(port, () => {
   console.log(`listening on ${port}`);
 })
